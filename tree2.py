8ffa7bb262286205871c82cab34bc5afb064c2f9
Max <max@recoset.com>
2015-11-21 20:31:28 -0500

    fixed version issues

---
+++ tree2.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+def find_tree_contour(gray, iterations=1):
+    _,thresh = cv2.threshold(gray, 192, 255, cv2.THRESH_BINARY_INV) # threshold
+    kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+    dilated = cv2.dilate(thresh,kernel,iterations = param) # dilate
+    im2, contours, hierarchy = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE) 
+
+    out = np.zeros(gray.shape, dtype=np.uint8) + 255
+
+    for i, contour in enumerate(contours):
+        [x,y,w,h] = cv2.boundingRect(contour)
+        if h < gray.shape[0] * 0.5 or w < gray.shape[1] * 0.5: continue
+        #cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,255), 1)
+        cv2.drawContours(out, contours, i, 0, -1)
+
+    return out
+
